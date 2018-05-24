d8c900b7feecae39fb5371ceb42013343110a178
James Y <jameshkyoo@gmail.com>
2018-05-24 09:43:30 -0400

    not sure what's happening

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const CELL_SIZE = 35;
 document.addEventListener("DOMContentLoaded", () => {
   const canvasEl = document.getElementById("canvas");
   const ctx = canvasEl.getContext("2d");
-  canvasEl.width = CELL_SIZE * 15;
-  canvasEl.height = CELL_SIZE * 15;
+  canvasEl.width = CELL_SIZE * 14;
+  canvasEl.height = CELL_SIZE * 14;
   const player1 = new Player("Player1");
   const player2 = new Player("Player2");
 
   const game = new Game(player1, player2);
+  window.game = game;
   const display = new Display(game, ctx);
   game.play();
 
-  setInterval(display.render, 100);
+  requestAnimationFrame(display.render);
 });
