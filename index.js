6b4a7a8a8db93bfe6053ba16f04c4ee26033e1cc
James Y <jameshkyoo@gmail.com>
2018-05-24 16:54:01 -0400

    getting back on track

--- index.js
+++ index.js
@@ -7,23 +7,22 @@ const CELL_SIZE = 35;
 document.addEventListener("DOMContentLoaded", () => {
   const canvasEl = document.getElementById("canvas");
   const ctx = canvasEl.getContext("2d");
-  canvasEl.width = CELL_SIZE * 14;
-  canvasEl.height = CELL_SIZE * 14;
+  canvasEl.width = CELL_SIZE * 14 + 1;
+  canvasEl.height = CELL_SIZE * 14 + 1;
   const player1 = new Player("Player1");
   const player2 = new Player("Player2");
   const game = new Game(player1, player2);
   window.game = game;
-  const display = new Display(game, ctx);
+  const display = new Display(game.board.grid, ctx);
 
   const clickHandler = (e) => {
     const xPosition = e.clientY;
     const yPosition = e.clientX;
-    console.log("in click handler");
     console.log([Math.round(xPosition/35), Math.round(yPosition/35)]);
     const move = [Math.round(xPosition/35), Math.round(yPosition/35)];
     try {
       game.board.placeStone(move);
-      display.render();
+      display.render(move);
       game.switchPlayer();
     } catch(error) {
       console.log(error);
@@ -32,6 +31,12 @@ document.addEventListener("DOMContentLoaded", () => {
     if (game.isOver()){
       console.log("Game over!");
     }
+
+    // else if (game.currentPlayer === "PLAYER2" && player2 instanceof Computer) {
+    //   console.log("Player2");
+    //   player2.getMove()
+    //   display.render()
+    // }
 
   };
 
