205f046155a6717431400cf3dca43a7b5cbbae56
James Y <jameshkyoo@gmail.com>
2018-05-23 23:54:47 -0400

    do I understand javascript?

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-import Board from "./lib/board";
 import Game from "./lib/game";
+import Display from "./lib/display";
+import Player from "./lib/player";
 
-window.game = new Game();
+const CELL_SIZE = 35;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const canvasEl = document.getElementById("canvas");
+  const ctx = canvasEl.getContext("2d");
+  canvasEl.width = CELL_SIZE * 15;
+  canvasEl.height = CELL_SIZE * 15;
+  const player1 = new Player("Player1");
+  const player2 = new Player("Player2");
+
+  const game = new Game(player1, player2);
+  const display = new Display(game, ctx);
+  game.play();
+
+  setInterval(display.render, 100);
+});
