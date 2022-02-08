4bd55ad6bb387130b477adb4cde7f32fb48721e4
AlexaLempel <Alexa.lempel@gmail.com>
2022-02-08 17:46:34 -0500

    removing extra lines

--- index.js
+++ index.js
@@ -16,31 +16,10 @@ document.addEventListener("DOMContentLoaded", () => {
   let game = new Game(player1, player2);
   let display = new Display(game.board.grid, ctx);
 
- /*  const setupModal = document.getElementById("setup-modal");
-  setupModal.classList.add("active-modal");
-  setupModal.addEventListener("click", () => setupModal.classList.remove("active-modal"));
-
-  const setupForm = document.getElementById("setup-form");
-  setupForm.addEventListener("click", e => e.stopPropagation());
-  setupForm.addEventListener("submit", e => {
-    e.preventDefault();
-    player2 = new Player("Player2");
-    game = new Game(player1, player2);
-    display = new Display(game.board.grid, ctx);
-    setupModal.classList.remove("active-modal");
-    canvasEl.addEventListener("mousedown", clickHandler);
-  });
-
-  const settingsButton = document.getElementById("settings-button");
-  settingsButton.addEventListener("click", e => {
-    e.preventDefault();
-    setupModal.classList.add("active-modal");
-  }); */
-
   const clickHandler = (e) => {
-    const xPosition = e.pageY - canvasEl.offsetTop - 35;
-    const yPosition = e.pageX - canvasEl.offsetLeft - 35;
-    const move = [Math.round(xPosition/35), Math.round(yPosition/35)];
+    const xPosition = e.pageY - canvasEl.offsetTop - CELL_SIZE ;
+    const yPosition = e.pageX - canvasEl.offsetLeft - CELL_SIZE ;
+    const move = [Math.round(xPosition/CELL_SIZE ), Math.round(yPosition/CELL_SIZE )];
 
     try {
       game.board.placeStone(move);
@@ -53,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
         canvasEl.removeEventListener("mousedown", clickHandler);
         setTimeout(() => {
           gameOverModal.classList.add("active-modal");
-        }, 1500);
+        }, 1000);
       }
     }
     catch(error) {
@@ -69,18 +48,6 @@ document.addEventListener("DOMContentLoaded", () => {
     gameOverModal.classList.remove("active-modal");
     canvasEl.addEventListener("mousedown", clickHandler);
   };
-
-  // const restartButton = document.getElementById("restart-button");
-  // restartButton.addEventListener("click", restartHandler);
-
-  // const yesButton = document.getElementById("yes");
-  // yesButton.addEventListener("click", restartHandler);
-
-  // const noButton = document.getElementById("no");
-  // noButton.addEventListener("click", e => {
-  //   e.preventDefault();
-  //   gameOverModal.classList.remove("active-modal");
-  // });
 
   canvasEl.addEventListener("mousedown", clickHandler);
 });
